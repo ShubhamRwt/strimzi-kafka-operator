3838a9def84c89694b20a97d7832b9c5e93374e6
ShubhamRwt <shubhamrwt02@gmail.com>
2023-08-29 15:21:13 +0530

    Unnecessory changes
    Signed-off-by: ShubhamRwt <shubhamrwt02@gmail.com>

--- cluster-operator/src/main/java/io/strimzi/operator/cluster/model/KafkaCluster.java
+++ cluster-operator/src/main/java/io/strimzi/operator/cluster/model/KafkaCluster.java
@@ -397,6 +397,7 @@ public class KafkaCluster extends AbstractModel implements SupportsMetrics, Supp
                 brokers.addAll(pool.nodes());
             }
         }
+
         return brokers;
     }
 
